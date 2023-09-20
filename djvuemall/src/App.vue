2bc6398abe43bd73e7ec993bda0cf9fcf6dc7820
dragon <1573511441@qq.com>
2023-09-20 14:32:31 +0800

    feat: core 多表联查操作

---
+++ djvuemall/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TestCore></TestCore>
+</template>
+
+<script>
+import TestCore from '@/components/TestCore.vue';
+export default {
+  name: 'App',
+  components: { TestCore },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ djvuemall/src/components/TestCore.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">sdafg</div>
+</template>
+
+<script>
+import { testCoreApi } from '@/restful/api';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  mounted() {
+    console.log(6666);
+    testCoreApi().then((res) => {
+      console.log(77, res);
+    });
+  },
+};
+</script>
