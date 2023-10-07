a7281e6e85dd78162fdfce901354e82a29b95db9
dragon <1573511441@qq.com>
2023-10-07 17:34:41 +0800

    feat: 图片管理，暂忽略

--- meiduo_py_vue/meiduo/meiduo_admin/urls.py
+++ meiduo_py_vue/meiduo/meiduo_admin/urls.py
@@ -5,6 +5,8 @@ from meiduo_admin.home import home_views
 from meiduo_admin.user import user_views
 from meiduo_admin.views import specs, options
 
+# from meiduo_admin.views import images
+
 urlpatterns = [
     # 后台系统的 登录 参考 https://www.jianshu.com/p/7ebf659c57a3
     path('authorizations/', MyTokenObtainPairView.as_view(), name='token_obtain_pair_cus'),  # 内部对 用户名和密码做了校验
@@ -25,9 +27,17 @@ urlpatterns = [
     # ------------规格路由表-----------两种方式都可以实现
     url(r'^goods/simple/$', specs.SpecsView.as_view({'get': 'simple'})),
     url(r'^goods/specs/simple/$', options.OptionSimple.as_view()),
+
+    # ------------图片路由————————————
+    #     url(r'^skus/simple/$', images.ImagesView.as_view({'get': 'simple'})),
 ]
 
 # ----------规格表路由------
 router = DefaultRouter()
 router.register('goods/specs', specs.SpecsView, basename='specs')
 urlpatterns += router.urls
+
+# -------图片表路由------
+# router = DefaultRouter()
+# router.register('skus/images', images.ImagesView, basename='images')
+# urlpatterns += router.urls
